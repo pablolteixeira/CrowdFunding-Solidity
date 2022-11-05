c05ed5f2dea940008869465b6f199d7185403a39
pablolteixeira <xpspablo@hotmail.com>
2022-11-05 14:33:53 -0300

    Configuration to deploy to heroku

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1250398064ec04f3"; }
+/******/ 		__webpack_require__.h = function() { return "a10ef37a09a665bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
