252b3ed0944d9eb4f1501fb73b2281a9d7f06ac3
pablolteixeira <xpspablo@hotmail.com>
2022-11-05 14:22:54 -0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "02bbfaf78e8909a3"; }
+/******/ 		__webpack_require__.h = function() { return "1250398064ec04f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/layout";
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/requestrow";
 
 
 
@@ -9,19 +11,65 @@ class RequestIndex extends Component {
     static async getInitialProps(props) {
         const { address } = props.query;
 
-        return { address };
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
+        const approversCount = await campaign.methods.approversCount().call();
+        
+        const request = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element, index) => {
+                return campaign.methods.requests(index).call();
+            })
+        );
+
+        return { address, request, requestCount, approversCount };
+    }
+
+    renderRows() {
+        return this.props.request.map((request, index) => {
+            return (
+            <RequestRow 
+                key={index}
+                id={index}
+                request={request}
+                address={this.props.address}
+                approversCount={this.props.approversCount}
+            />
+            ); 
+        })
     }
 
     render () {
+        const { Header, Row, HeaderCell, Body } = Table;
+
         return (
             <Layout>
+                <h3>Requests</h3>
                 <Link route={`/campaigns/${this.props.address}/requests/new`}>
                     <a>
-                        <Button primary>
+                        <Button primary floated="right" style={{ marginBottom: 10 }}>
                             Add Request
                         </Button>
                     </a>
                 </Link>
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval Count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                    <Body>
+                        {this.renderRows()}
+                    </Body>
+                </Table>
+                <div>
+                    Found {this.props.requestCount} requests.
+                </div>
             </Layout>
         );
     }
